e4e77a6da874958894e99d33e00524caebf3fa49
Jordan Jones <jordanalanjones@gmail.com>
2018-04-24 15:13:02 -0500

    Adding add class to event when creating event

--- src/app/admin/events/event-form/event-form.component.ts
+++ src/app/admin/events/event-form/event-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Event } from '../../../entity/Event';
 import { EventService } from '../../../services/event.service';
+import { ClassService } from '../../../services/class.service';
 
 @Component({
   selector: 'event-form',
@@ -12,11 +13,15 @@ export class EventFormComponent implements OnInit {
   @Input() event: Event;
   @Input() updateForm;
 
+  classes;
+
   constructor(
-    private eventService: EventService
+    private eventService: EventService,
+    private classService: ClassService
   ) { }
 
   ngOnInit() {
+    this.getAllClasses();
   }
 
   performAction() {
@@ -45,6 +50,29 @@ export class EventFormComponent implements OnInit {
 
   delete() {
     this.eventService.delete(this.event).subscribe();
+  }
+
+  getAllClasses() {
+    this.classService.getAllClasses().subscribe(
+      data => { this.classes = data; console.log(data);},
+      error => console.log(error);
+    );
+  }
+
+  addOrRemoveClass(_class: Class) {
+    if(this.event.classes == undefined) {
+      this.event.classes = [];
+    }
+
+    let index = this.event.classes.indexOf(_class);
+
+    if(index < 0) {
+      this.event.classes.push(_class);
+    } else {
+      this.event.classes.splice(index, 1);
+      console.log("need to remove");
+    }
+
   }
 
 }
